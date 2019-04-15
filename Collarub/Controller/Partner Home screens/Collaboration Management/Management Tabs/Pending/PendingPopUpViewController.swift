772c8a11107ddb6a77f1d6da467538c68cdbba48
gotozainishaikh <to.zainishaikh@gmail.com>
2019-04-15 15:25:15 +0500

    new commit

---
+++ Collarub/Controller/Partner Home screens/Collaboration Management/Management Tabs/Pending/PendingPopUpViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PendingPopUpViewController.swift
+//  Collarub
+//
+//  Created by mac on 13/04/2019.
+//  Copyright © 2019 Mac 1. All rights reserved.
+//
+
+import UIKit
+
+class PendingPopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        showAnimate()
+    }
+    
+
+    func showAnimate()
+    {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    @IBAction func cancel(_ sender: UIButton) {
+        view.removeFromSuperview()
+    }
+    @IBAction func productSentBtn(_ sender: Any) {
+        let alert = UIAlertController(title: "Alert", message: "Are you you have sent product", preferredStyle: .alert
+        )
+        
+        let ok = UIAlertAction(title: "Yes", style: .default) { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .default) { (cancel) in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        
+     present(alert, animated: true, completion: nil)
+    }
+    
+}
